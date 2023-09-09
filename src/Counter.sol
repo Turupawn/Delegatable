28fe88dcb3b5cd2a1cd4c3002dbbe504cfefedde
turupawn <ahmed.hn.43@gmail.com>
2023-09-09 14:33:16 -0600

    added igovernor stuff

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,36 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+interface IGovernor is Ownable {
+    function propose(
+        address[] memory targets,
+        uint256[] memory values,
+        bytes[] memory calldatas,
+        string memory description
+    ) external returns (uint256 proposalId);
+
+    function castVoteWithReason(
+        uint256 proposalId,
+        uint8 support,
+        string calldata reason
+    ) external returns (uint256 balance);
+}
+
+contract Counter {
+    address public delegate;
+
+    function setDelegate(address delegate_) onlyOwner {
+        delegate = delegate_;
     }
 
-    function increment() public {
-        number++;
+    function castVote(
+        address governorAddress,
+        uint256 proposalId,
+        uint8 support,
+        string calldata reason) {
+        require(msg.sender == delegate, "Only delegate can cast votes");
+        IGovernor(governorAddress).castVoteWithReason(proposalId, support, reason);
     }
 }
